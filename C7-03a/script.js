298acd5b08550404feadf48547e52bcb6b1d02aa
原田 悠平 <114724659+Harada-TECH@users.noreply.github.com>
2026-01-30 18:35:28 +0900

    Add files via upload

---
+++ C7-03a/script.js
@@ -0,0 +1,28 @@
+// ページ内の .fade-inを全て取得
+const fadeInElements = document.querySelectorAll('.fade-in');
+
+// Intersection Observer のオプション設定
+const options = {
+  root: null,         // ビューポートをルート要素とする
+  rootMargin: "0px",  // オフセット無し
+  threshold: 0.1      // 要素が10%見えたらコールバックを実行
+}
+
+// Intersection Observer のコールバック
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // 要素が見えたらクラスを追加
+      entry.target.classList.add("show");
+
+      // 一度アニメーションが完了したら、再び監視しない場合は unobserve
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+// 上記コールバックとオプションを使ってObserverを生成
+const observer = new IntersectionObserver(callback, options);
+
+// 取得した要素それぞれをObserverに登録
+fadeInElements.forEach(el => observer.observe(el));
